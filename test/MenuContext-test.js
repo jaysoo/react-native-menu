c350277f7a21ee8d4c757249443d0a84712ef7e4
Jack Hsu <jack.hsu@gmail.com>
2016-01-15 11:30:43 -0500

    init with some tests

---
+++ test/MenuContext-test.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const React = require('react');
+const expect = require('chai').expect;
+const TestUtils = require('react-addons-test-utils');
+const sinon = require('sinon');
+const makeMenuContext = require('../src/menu/makeMenuContext');
+
+describe('MenuContext', () => {
+  let MenuContext, renderer, UIManager;
+
+  beforeEach(() => {
+    renderer = TestUtils.createRenderer();
+
+    const Dimensions = {
+      get: () => ({ width: 360, height: 640 })
+    };
+
+    UIManager = {
+      fakedMeasurements: {
+        left: 0, top: 0, width: 360, height: 640, pageX: 0, pageY: 0
+      },
+      // Immediately invokes the callback with our faked measurements.
+      measure: (_, callback) => {
+        const { left, top, width, height, pageX, pageY } = UIManager.fakedMeasurements;
+        callback(left, top, width, height, pageX, pageY);
+      }
+    };
+
+    const StyleSheet = { create: x => x };
+
+    const ReactStub = {
+      ...React,
+      Dimensions,
+      NativeModules: { UIManager },
+      StyleSheet
+    };
+
+    const moduleConfig = {
+      constants: require('../src/menu/constants'),
+      model: require('../src/menu/makeModel')(ReactStub),
+      styles: require('../src/menu/makeStyles')(ReactStub)
+    };
+
+    MenuContext = makeMenuContext(ReactStub, moduleConfig);
+  });
+
+  it('renders', () => {
+    renderer.render(<MenuContext/>);
+    const menuContext = renderer.getRenderOutput();
+    expect(menuContext).to.be.ok;
+  });
+});
