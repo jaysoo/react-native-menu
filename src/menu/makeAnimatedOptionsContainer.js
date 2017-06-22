1f9d8d288a03479fc7ebc1c622ebdb77d17fa42d
Amen Souissi <amensouissi@ecreall.com>
2017-06-22 13:38:45 +0200

    fix Android event flashing menu

--- src/menu/makeAnimatedOptionsContainer.js
+++ src/menu/makeAnimatedOptionsContainer.js
@@ -1,29 +1,35 @@
 module.exports = (React, ReactNative) => {
-  const { Animated } = ReactNative;
-  const TimerMixin = require('react-timer-mixin');
-
+  const { Animated, Platform } = ReactNative
+  const TimerMixin = require("react-timer-mixin")
   // A component that scales in when mounted.
   const AnimatedOptionsContainer = React.createClass({
     mixins: [TimerMixin],
     getInitialState() {
-      return { scaleAnim: new Animated.Value(0.001) };
+      return {
+        scaleAnim: new Animated.Value(Platform.OS === "android" ? 0.01 : 0.001)
+      }
     },
     componentDidMount() {
       this.setTimeout(() => {
         Animated.timing(this.state.scaleAnim, {
           duration: 60,
           toValue: 1
-        }).start();
-      }, 16);
+        }).start()
+      }, 16)
     },
     render() {
       return (
-        <Animated.View style={[this.props.style, { transform: [ { scale: this.state.scaleAnim } ] }]}>
-          { this.props.children }
+        <Animated.View
+          style={[
+            this.props.style,
+            { transform: [{ scale: this.state.scaleAnim }] }
+          ]}
+        >
+          {this.props.children}
         </Animated.View>
-      );
+      )
     }
-  });
+  })
 
-  return AnimatedOptionsContainer;
-};
+  return AnimatedOptionsContainer
+}
